9180d2747ef0a45bbeb56e2811693446c72a418b
Maria Clara <mcbarroso.contato@gmail.com>
2022-06-26 19:42:29 -0300

    Learn about logic

--- exercicios/src/fundamentos/operadores/Logicos.java
+++ exercicios/src/fundamentos/operadores/Logicos.java
@@ -2,6 +2,60 @@ package fundamentos.operadores;
 
 public class Logicos {
 	public static void main(String[] args) {
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
 		
+		System.out.println(condicao1 && condicao2); // !condicao2 -> Verdadeiro, logo a expressao seria verdadeira
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2);
+		System.out.println(!condicao1); // !!condicao1 -> Verdadeiro, ja que o não nega na negação
+		System.out.println(!condicao2);
+		
+		// Tabela verdade
+		/* Condicao E
+		 * V	V	->	V
+		 * V	F	->	F
+		 * F	V	->	F
+		 * F	F	->	F
+		 * 
+		 * Condicao OU
+		 * V	V	->	V
+		 * V	F	->	V
+		 * F	V	->	V
+		 * F	F	->	F
+		 * 
+		 * Condicao XOR
+		 * V	V	->	F
+		 * V	F	->	V
+		 * F	V	->	V
+		 * F	F	->	F
+		 * 
+		 * Condicao NOT
+		 * NOT V	->	F
+		 * NOT F	->	V
+		 * */
+		System.out.println("Tabela verdade E");
+		System.out.println(true && true);
+		System.out.println(true && false);
+//		System.out.println(false && true);
+//		System.out.println(false && true);
+		
+		System.out.println("Tabela verdade OU");
+//		System.out.println(true || true);
+//		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		
+		System.out.println("Tabela verdade XOR");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+		
+		System.out.println("Tabela verdade do NOT");
+		System.out.println(!true);
+		System.out.println(!!true);
+		System.out.println(!false);
+		System.out.println(!!false);
 	}
 }
