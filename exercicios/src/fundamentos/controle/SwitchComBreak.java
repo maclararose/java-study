697b8bcf1375559f298017c19bcab021dbd658a7
mcbarroso <mcbarroso.contato@gmail.com>
2023-05-04 23:05:41 -0300

    Add new files about control structs

---
+++ exercicios/src/fundamentos/controle/SwitchComBreak.java
@@ -0,0 +1,43 @@
+package fundamentos.controle;
+
+import java.util.Scanner;
+
+public class SwitchComBreak {
+
+	public static void main(String[] args) {
+		System.out.println("Digite uma nota: ");
+		Scanner scan = new Scanner(System.in);
+
+		int nota = scan.nextInt();
+		scan.close();
+
+		String conceito = "";
+
+		switch (nota) {
+		case 10:
+		case 9:
+			conceito = "A";
+			break;
+		case 8:
+		case 7:
+			conceito = "B";
+			break;
+		case 6:
+		case 5:
+			conceito = "C";
+			break;
+		case 4:
+		case 3:
+			conceito = "D";
+		case 2:
+		case 1:
+		case 0:
+			conceito = "F";
+			break;
+		default:
+			System.out.println("Nota não valida");
+		}
+		System.out.println("Conceito: " + conceito);
+	}
+
+}
