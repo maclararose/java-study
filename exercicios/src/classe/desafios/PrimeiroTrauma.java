6e9fad1ac866fc1ff16a436d649ba58188aa4af2
mcbarroso <mcbarroso.contato@gmail.com>
2023-05-14 01:28:47 -0300

    Add annotations

--- exercicios/src/classe/desafios/PrimeiroTrauma.java
+++ exercicios/src/classe/desafios/PrimeiroTrauma.java
@@ -2,10 +2,12 @@ package classe.desafios;
 
 public class PrimeiroTrauma {
 	int a = 3;
+	static int b = 6;
 	
 	public static void main(String[] args) {
 		PrimeiroTrauma a = new PrimeiroTrauma();
 		System.out.println(a.a);
+		System.out.println(b); // algo estatico consegue acessar outro estatico
 	}
 
 }
