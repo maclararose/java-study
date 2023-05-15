7a67bd77c6d10dabaf1e6969c181f365df34c115
mcbarroso <mcbarroso.contato@gmail.com>
2023-05-15 20:53:07 -0300

    Class about empty and null

---
+++ exercicios/src/classe/ValorNulo.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class ValorNulo {
+
+	public static void main(String[] args) {
+		String s1 = "";
+		System.out.println(s1.concat("!!!!!!"));
+		
+		String s2 = null;
+		System.out.println(s2.concat("********"));
+
+	}
+
+}
