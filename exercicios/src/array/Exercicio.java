bbb8db1805bf2b372846c33202b9eaf44aacc045
mcbarroso <mcbarroso.contato@gmail.com>
2023-05-18 09:22:11 -0300

    Class about foreach

--- exercicios/src/array/Exercicio.java
+++ exercicios/src/array/Exercicio.java
@@ -1,4 +1,4 @@
-package array.exercicio;
+package array;
 
 import java.util.Arrays;
 
---
+++ exercicios/src/array/Foreach.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		double[] notas = {9.9, 8.66, 7.5, 6.8};
+		
+		for (double d : notas) {
+			System.out.println(d);
+		}
+
+	}
+
+}
