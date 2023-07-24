62a48a24ab76112097e6126361ee5fd075571912
mcbarroso <mcbarroso.contato@gmail.com>
2023-07-24 13:26:36 -0300

    Add file about Set

--- exercicios/src/colecoes/ConjuntoBaguncado.java
+++ exercicios/src/colecoes/ConjuntoBaguncado.java
@@ -1,9 +1,11 @@
 package colecoes;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class ConjuntoBaguncado {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		HashSet conjunto = new HashSet();
 		
@@ -27,6 +29,26 @@ public class ConjuntoBaguncado {
 		System.out.println(conjunto.size());
 		
 		System.out.println(conjunto.contains('x'));
+		System.out.println(conjunto.contains(1));
+		System.out.println(conjunto.contains(true));
+		
+		Set nums = new HashSet();
+		
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		//conjunto.addAll(nums); // -> uniao de dois conjuntos
+		
+		System.out.println(nums);
+		System.out.println(conjunto);
+		
+		conjunto.retainAll(nums);
+		
+		System.out.println(conjunto);
+		
+		conjunto.clear();
+		System.out.println(conjunto);
 
 	}
 
