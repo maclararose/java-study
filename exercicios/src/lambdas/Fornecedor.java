165094a387d5740ff658e098a75d073cff906abf
mcbarroso <mcbarroso.contato@gmail.com>
2023-08-14 18:58:33 -0300

    Add file about the Supplier class

---
+++ exercicios/src/lambdas/Fornecedor.java
@@ -0,0 +1,17 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+	public static void main(String[] args) {
+		Supplier<List<String>> umaLista =
+				() -> Arrays.asList("Ana", "Lia", "Gui", "Bia");
+				
+		System.out.println(umaLista.get());
+
+	}
+
+}
